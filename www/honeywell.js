4fe10059492c42132a65df3f0a017ac272f93994
chandys <chandbecse@gmail.com>
2018-08-28 18:11:54 +0530

    Update honeywell.js

--- www/honeywell.js
+++ www/honeywell.js
@@ -1,25 +1,14 @@
 var cordova = require('cordova');
 var execute = require('cordova/exec');
 
-var honeywell = {
-    listen: function (res, err) {
+
+    module.exports.listen = function (res, err) {
         return execute(res, err, "HoneywellScannerPlugin", "listen", []);
     },
-    release: function () {
+    module.exports.release = function () {
         return execute(null, null, "HoneywellScannerPlugin", "release", []);
     },
-    claim: function () {
+    module.exports.claim = function () {
         return execute(null, null, "HoneywellScannerPlugin", "claim", []);
     }
-    // @todo:
-    /*
-    enable: function (res, err) {
-        return exe(res, err, "HoneywellScannerPlugin", "enableTrigger", []);
-    },
-    disable: function (res, err) {
-        return exe(res, err, "HoneywellScannerPlugin", "disableTrigger", []);
-    }
-    */
-};
-
-module.exports = honeywell;
+    
